8ae0200654084a23d777d8774b7ad257450fdffd
ethangoldstein <ethanjacobgoldstein@gmail.com>
2016-06-08 19:13:37 -0700

    Add files via upload
    Improved, and proves that there is small error in the algorithm currently employed

--- Ideal_FFT.m
+++ Ideal_FFT.m
@@ -1,30 +1,46 @@
 %http://www.mathworks.com/help/fixedpoint/ug/convert-fast-fourier-transform-fft-to-fixed-point.html
 %clc;
-%start of matlab FFT smaple code
+clf;
+
+%start of matlab FFT sample code
 n = 1024;                                     % Number of points
 Fs = 4;                                     % Sampling frequency in Hz
 t  = (0:(n-1))/Fs;                          % Time vector
 f  = linspace(0,Fs,n);                      % Frequency vector
 f1 = 2; f2 = 30;                           % Frequencies, in Hz
-a1 = 20; a2 = 40;
+a1 = 1000; a2 = 40;
 %x0 = cos(2*pi*f0*t) + 0.55*cos(2*pi*f1*t);  % Time-domain signal
 for i=1:n
-    x0(i) = a1*cos(f1*2.0*(i-1)*3.1415926535/n)+a2*cos(f2*2.0*(i-1)*3.1415926535/n) + 100;
+    x0(i) = a1*cos(f1*2.0*(i-1)*3.1415926535/n);
 end
 x0 = complex(x0);                           % The textbook algorithm requires the input to be complex
-
 w0 = fidemo.fi_radix2twiddles(n);
-y0 = fft(x0); % MATLAB's built-in FFT for comparison
 x = sfi(x0);
 w = sfi(w0);
+y = fi_m_radix2fft_withscaling(x,w); %contains the Ideal Integer FFT
 
-y = fi_m_radix2fft_withscaling(x,w);
-fidemo.fi_fft_demo_plot(real(x), y, y0/n, n, 'Fixed-point data',{'Fixed-point FFT with scaling','Scaled built-in'});
+%Calculating Micro FFT based on imported Data
+re = Micro_FFT(:,1);
+im = Micro_FFT(:,2);
+amp = sqrt(re.^2 + im.^2);
 
-re = test2(:,1);
-im = test2(:,2);
-x = 0:1:length(re)-1;
+%Plotting all the Data
+samp = 0:1:length(x)-1;
 
-amp = sqrt(re.^2 + im.^2)
+subplot(4,1,1)
+plot(samp,real(x));
+title('Input Freqeuncy Time Domain')
 
-plot(x,amp)
+subplot(4,1,2) 
+plot(samp,abs(y));
+title('Matlab IntFFT');
+
+subplot(4,1,3);
+plot(samp,amp);
+title('Micro IntFFT');
+
+subplot(4,1,4);
+plot(samp,abs(y) - transpose(amp));
+title('The Difference');
+
+
